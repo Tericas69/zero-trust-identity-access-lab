102778d1d15947aafe9d7ef75d708ddc157956b7
Terica K Shepard <143021237+Tericas69@users.noreply.github.com>
2026-04-05 23:57:57 -0500

    Add role-based routes

--- app/app.py
+++ app/app.py
@@ -3,10 +3,27 @@ from data import load_users
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     users = load_users()
     return render_template("home.html", users=users)
+
+
+@app.route("/student")
+def student():
+    return render_template("student.html")
+
+
+@app.route("/staff")
+def staff():
+    return render_template("staff.html")
+
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
