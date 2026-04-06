2a597187a20d2fa169d42285321d2e6e07e4fdc9
Terica K Shepard <143021237+Tericas69@users.noreply.github.com>
2026-04-05 23:45:45 -0500

    Load user data in home route

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from data import load_users
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    users = load_users()
+    return render_template("home.html", users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
